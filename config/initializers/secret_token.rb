930f8d62fe9d4090eaf1561a672ce25b6d79e761
DylanWyer <liveffs@gmai.comgit config --global github.user dylan.wyer>
2013-05-10 11:04:08 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blogsite::Application.config.secret_key_base = '997b33c2af8ac6e098c4157c32aa19c678da360ed7ba59ebdbd04beb7405b8035bc015cd56fb83a2454cf179edda98c13e3418525a8ac1dcfe101e5affeca5f1'
